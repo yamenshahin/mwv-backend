95e0887a832e6e94e6b5d13ba5de006c205ae9f6
Yamen Shahin <yamenshahin@gmail.com>
2019-09-30 01:09:16 +0200

    Add admin login

---
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Auth;
+
+class AdminLoginController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest:admin')->except('logout');
+    }
+
+    public function showLoginForm()
+    {
+        return view('auth.admin-login');
+    }
+
+    public function login(Request $request)
+    {
+        $this->validate($request,[
+            'email' => 'required|email',
+            'password'  => 'required'
+        ]);
+        if(Auth::guard('admin')->attempt($request->only('email', 'password'), $request->remember)){
+            return redirect()->intended('admin.dashboard');
+        }
+        
+        return redirect()->back()->withInput($request->only('email', 'remember'));
+    }
+
+    /**
+     * Log the user out of the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function logout(Request $request)
+    {
+        Auth::guard('admin')->logout();
+
+        $request->session()->invalidate();
+
+        return redirect('/');
+    }
+}
--- app/Http/Requests/UserLoginRequest.php
+++ app/Http/Requests/UserLoginRequest.php
@@ -24,7 +24,7 @@ class UserLoginRequest extends FormRequest
     public function rules()
     {
         return [
-            'email' => 'required',
+            'email' => 'required|email',
             'password'  => 'required'
         ];
     }
